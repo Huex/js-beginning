a561d08904c1e87f0f2a4d90db6b037a2d54ba15
Huex <tohuex@gmail.com>
2018-03-08 19:52:26 +0300

    Init

---
+++ JsonConverter/JSON Converter/js/main.js
@@ -0,0 +1,35 @@
+﻿'use strict';
+
+var defaultData =
+{
+    title: "JSON Converter"
+};
+
+angular.module("jsonConverter", []).controller("pageCtrl", [
+    "$scope", function($scope) {
+        $scope.title = defaultData.title;
+        var jsonDoc = {};
+
+        function convert(doc) {
+            for (var i = 0; i < doc.length; i++) {
+                doc[i].collections = [{ id: doc[i].Type }];
+                doc[i].id = doc[i].Id;
+                delete doc[i].Type;
+                delete doc[i].Id;
+            }
+            return doc;
+        }
+        function handleFileSelect(evt) {
+            var files = evt.target.files;
+            for (var i = 0, f; f = files[i]; i++) {
+                var reader = new FileReader();
+                reader.onload = (function(theFile) {
+                    jsonDoc = JSON.parse(reader.result);
+                    console.log(JSON.stringify(convert(jsonDoc)));
+                });
+                reader.readAsText(f);
+            }
+        }
+        document.getElementById('files').addEventListener('change', handleFileSelect, false);
+    }
+]);
---
+++ Task Manager/WebAplication/WebAplication/js/main.js
@@ -0,0 +1,42 @@
+﻿(function () {
+    var passwordDb = "SyncMaster";
+    var loginDb = "hu3x";
+    var wepAplicationApp = angular.module("taskManagerApp", []);
+    wepAplicationApp.controller("taskManagerCtrl", ["$scope", function ($scope) {
+        var taskManager = $scope;
+        var db = databoom("https://t398.databoom.space", "b398");
+
+        taskManager.upDate = function() {
+            db.login(loginDb,passwordDb).then(function() {
+                    db.load("tasks").then(function(data) {
+                        taskManager.tasks = data;
+                        $scope.$apply();
+                    });
+                }
+            );
+        }
+
+        taskManager.title = "Task Manager";
+        taskManager.user = "Stas";
+        taskManager.upDate();
+        taskManager.addNewTask = function (taskName) {
+            if (taskName !== undefined) {
+                var task = {
+                    name: taskName,
+                    status: false
+                };
+                taskManager.tasks.push(task);
+                db.login(loginDb, passwordDb).then(function() {
+                    db.save("tasks", task);
+                });
+                taskManager.taskName = undefined;
+            }
+        }
+        taskManager.setStyle = function (status) {
+            return status ? "color : green" : "color : red; font-weight : bold";
+        }
+        taskManager.showText = function (status) {
+            return status ? "Выполнено" : "В процессе";
+        } 
+    }]);
+})();
